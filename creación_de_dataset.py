591936b569d13558ff7927c7cf7b65072b11d810
Aldhair <134818541+AldhairHr14@users.noreply.github.com>
2025-06-09 08:48:15 -0500

    creación_de_dataset.py

--- creación_de_dataset.py
+++ creación_de_dataset.py
@@ -2,7 +2,7 @@ import psutil
 import pandas as pd
 from datetime import datetime
 
-# Crear lista de procesos
+
 procesos = []
 
 for proc in psutil.process_iter(['pid', 'name', 'username', 'cpu_percent', 'memory_percent']):
@@ -13,10 +13,10 @@ for proc in psutil.process_iter(['pid', 'name', 'username', 'cpu_percent', 'memo
     except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
         continue
 
-# Crear DataFrame
+
 df = pd.DataFrame(procesos)
 
-# Guardar como CSV
+
 nombre_archivo = "procesos_activos.csv"
 df.to_csv(nombre_archivo, index=False)
 
