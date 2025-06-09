1e7e5529a38913a286e52328d80b9f01455f10df
Aldhair <134818541+AldhairHr14@users.noreply.github.com>
2025-06-09 08:47:46 -0500

    análisis_del_dataset.py

--- análisis_del_dataset.py
+++ análisis_del_dataset.py
@@ -3,21 +3,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Enlace público del CSV (reemplázalo con el tuyo)
+
 url = "https://raw.githubusercontent.com/AldhairHr14/Dataset/refs/heads/main/procesos_activos.csv"
 
-# Leer dataset
+
 df = pd.read_csv(url)
 
-# Mostrar primeras filas
+
 print("Primeras filas del dataset:")
 print(df.head())
 
-# Estadísticas básicas
+
 print("\nEstadísticas básicas:")
 print(df[['cpu_percent', 'memory_percent']].describe())
 
-# Media, mediana, moda, desviación estándar
+
 print("\nMedia:")
 print(df[['cpu_percent', 'memory_percent']].mean())
 
@@ -30,12 +30,12 @@ print(df[['cpu_percent', 'memory_percent']].mode().iloc[0])
 print("\nDesviación estándar:")
 print(df[['cpu_percent', 'memory_percent']].std())
 
-# Matriz de correlación
+
 print("\nMatriz de correlación:")
 correlacion = df[['cpu_percent', 'memory_percent']].corr()
 print(correlacion)
 
-# Visualización (opcional)
+
 sns.heatmap(correlacion, annot=True, cmap="coolwarm")
 plt.title("Matriz de Correlación")
 plt.show()
